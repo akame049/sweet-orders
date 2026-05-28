dc26f437c2de3514ff603f6a8e76160103f7c66c
akame049 <maia.braicu@gmail.com>
2026-05-28 12:54:57 +0300

    Fix: configurat Sequelize sa foloseasca DB_HOST si DB_NAME in productie

--- server/models/index.js
+++ server/models/index.js
@@ -10,10 +10,23 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (process.env.DB_NAME) {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASS,
+        {
+            host: process.env.DB_HOST,
+            dialect: config.dialect || 'mysql',
+            port: process.env.DB_PORT || 3306,
+            logging: false
+        }
+    );
+} else if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
